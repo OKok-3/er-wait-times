b785a939f1dcf802546c46787c56c4f911e2c5bf
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2025-10-29 18:07:44 -0400

    feat: implement data loader to postgres database

--- airflow/open_wait_times/scrapers/lhsc.py
+++ airflow/open_wait_times/scrapers/lhsc.py
@@ -1,7 +1,10 @@
 import re
+import pandas as pd
 from .base import BaseScraper
+from pendulum import duration, parse
 from httpx import get
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 
 class LHSC(BaseScraper):
@@ -19,7 +22,7 @@ class LHSC(BaseScraper):
 
         return {
             "filename": filename,
-            "task_timestamp": ts,
+            "fetch_ts": ts,
         }
 
     def parse(self, data: dict[str, str]) -> dict[str, any]:
@@ -37,10 +40,45 @@ class LHSC(BaseScraper):
         wait_time_match = re.search(wait_time_re, html)
         time_updated_match = re.search(time_updated_re, html)
 
-        data["wait_time"] = wait_time_match.group(1).strip() if wait_time_match else None
-        data["time_updated"] = time_updated_match.group(1).strip() if time_updated_match else None
+        data["wait_duration"] = wait_time_match.group(1).strip() if wait_time_match else None
+        data["update_ts"] = time_updated_match.group(1).strip() if time_updated_match else None
 
         return data
 
     def save(self, data: dict[str, any]) -> None:
-        pass
+        pg = PostgresHook(postgres_conn_id="owt-pg")
+
+        # We won't do None/Null checks and will rely on pipeline failing to catch this
+        hospital_id = self._id
+        fetch_ts = parse(data["fetch_ts"], strict=False)
+        update_ts = parse(data["update_ts"], strict=False, tz="America/Toronto")
+
+        wait_duration = pd.to_timedelta(data["wait_duration"].lower().strip())
+        patient_arrival_time = update_ts.subtract(minutes=int(wait_duration.total_seconds() / 60))
+        patient_departure_time = update_ts
+        extra_info = None
+
+        # TODO: Conflict will be dealt with by the to-be-implemented file hash checking step
+        pg.insert_rows(
+            table="owt.er_wait_times",
+            rows=[
+                (
+                    hospital_id,
+                    fetch_ts,
+                    update_ts,
+                    wait_duration,
+                    patient_arrival_time,
+                    patient_departure_time,
+                    extra_info,
+                )
+            ],
+            target_fields=[
+                "hospital_id",
+                "fetch_ts",
+                "update_ts",
+                "wait_duration",
+                "patient_arrival_time",
+                "patient_departure_time",
+                "extra_info",
+            ],
+        )
